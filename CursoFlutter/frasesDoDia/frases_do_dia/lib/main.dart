cd34d330390409483ce4c05c422466c14f87e78c
John-Rocha <52468127+John-Rocha@users.noreply.github.com>
2020-03-30 22:21:20 -0300

    Curso Flutter

---
+++ CursoFlutter/frasesDoDia/frases_do_dia/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    //debugShowCheckedModeBanner: false,
+    title: "Frases do Dia",
+    home: Container(
+      margin: EdgeInsets.only(top: 40),
+      decoration: BoxDecoration(
+        border: Border.all(width: 3, color: Colors.white),
+      ),
+      child: Image.asset(
+        "images/mesa.jpg",
+        fit: BoxFit.scaleDown,
+      ),
+    ),
+  ));
+}
