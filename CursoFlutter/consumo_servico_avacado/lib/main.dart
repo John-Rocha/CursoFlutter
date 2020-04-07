fd21fb960da11aca7ba1bad542410cf5adeb8cf7
JohnRocha <johnathanrocha@gmail.com>
2020-04-07 00:11:38 -0300

    Flutter

---
+++ CursoFlutter/consumo_servico_avacado/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:consumo_servico_avacado/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Home(),
+));
