8cf0fd23d210268bf463286acfecb3ff8a05841a
John-Rocha <52468127+John-Rocha@users.noreply.github.com>
2020-04-16 00:14:52 -0300

    Flutter

---
+++ CursoFlutter/projeto_pessoal/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_pessoal/models/item.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  theme: new ThemeData(
+    brightness:Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        primaryColor: const Color(0xFF1bbabf),
+        accentColor: const Color(0xFF64ffda),
+        canvasColor: const Color(0xFF303030),
+        fontFamily: 'Merriweather',
+  ),
+  home: Home(),
+));
+
+class Home extends StatefulWidget {
+
+  var itens = new List<Item>();
+
+  Home() {
+    itens = [];
+    itens.add(Item(title: "Banana", done: false));
+    itens.add(Item(title: "Abacate", done: true));
+    itens.add(Item(title: "Laranja", done: false));
+  }
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var text = TextEditingController();
+
+  void add() {
+    if (text.text.isEmpty) {
+      return;
+    }
+    setState(() {
+      widget.itens.add(Item(title: text.text, done: false));
+      text.clear();
+    });
+  }
+
+  void _remove(int index) {
+    widget.itens.removeAt(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(
+              color: Colors.white
+            )
+          ),
+          controller: text,
+        ),
+      ),
+      body: ListView.builder(
+        //Quantidade de itens da lista
+        itemCount: widget.itens.length,
+
+        //Função que determina como eu vou construir essa lista para exibição precisa de dois parâmetros (BuildContext e o indice do array)
+        itemBuilder: (context, index) {
+          final item = widget.itens[index];
+          return Dismissible(
+            key: Key(item.title),
+            child: CheckboxListTile(
+            title: Text(item.title),
+            value: item.done,
+            onChanged: (valor) {
+              setState(() {
+                item.done = valor;
+                print(valor);
+              });
+            },
+          ),
+          onDismissed: (direction) {
+            remove(index);
+          },
+          background: Container(
+            color: Colors.red.withOpacity(0.2),
+          ),
+          );
+        }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Color(0xFF64ffda),
+        onPressed: () => add(),
+      ),
+    );
+  }
+}
