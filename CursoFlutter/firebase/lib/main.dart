7c9e7e15be9288ed16a79485eb3cdaf2fd796176
John-Rocha <52468127+John-Rocha@users.noreply.github.com>
2020-05-01 23:57:14 -0300

    Firebase

--- CursoFlutter/firebase/lib/main.dart
+++ CursoFlutter/firebase/lib/main.dart
@@ -1,43 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Home(),
-));
+void main() async {
 
-class Home extends StatefulWidget {
+  WidgetsFlutterBinding.ensureInitialized ();
+
+  Firestore db = Firestore.instance;
+
+  /* 
+  db.collection("usuarios")
+    .document("003")
+    .setData(
+      {
+        "nome": "Maria",
+        "idade": "53"
+      }
+    );   
+    */
+
+  /* 
+  Firestore db = Firestore.instance;
+
+  DocumentSnapshot snapshot = await db.collection("usuarios").document("002").get();
+
+  print("Dados: " + snapshot.data.toString());
+  */  
+
+  QuerySnapshot querySnapshot = await db
+    .collection("usuarios")
+    .getDocuments();
+
+  //print("dados usuarios: " + querySnapshot.documents.toString());
+
+  for (DocumentSnapshot item in querySnapshot.documents) {
+    print("dados usuarios: " + item.);
+  }
+
+runApp(Home());
+} 
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+/* class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
 
-  Firestore db = Firestore.instance;
 
+  _recuperandoDados() async {
+
+    Firestore db = Firestore.instance;
+    DocumentSnapshot snapshot = await db.collection("usuarios").document("001").get();
+
+    print("Dados: " + snapshot.data.toString());
+
+  }
+
+  _deletandoDados() {
+    Firestore db = Firestore.instance;
+    db.collection("usuarios").document("001").delete();
+  }
 
   @override
   void initState() {
     super.initState();
-    db.collection("noticias")
-      .document("BNXYEMjGxGUxPhEQFkbv")
-      .setData(
-        {
-          "titulo": "Flutter é legal",
-          "descricao": "Segunda noticia!"
-        }
-      );
+    _recuperandoDados();
   }
+  
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Testando Firebase"),
       ),
-      body: Center(
+      body: ListView.builder(
         
       ),
     );
   }
 }
+*/
